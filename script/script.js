13f448d2a048cddd4967496f1a04c8a1087b53b6
ChrisLJewell <143720772+ChrisLJewell@users.noreply.github.com>
2026-03-13 17:17:15 -0400

    Website upload
    Portfolio website upload

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function feature_toggle() {
+    const list = document.getElementById("features_list");
+    list.style.display = (list.style.display === "block") ? "none" : "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
